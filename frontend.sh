a2423dfbf1f80a86ee44c7cf0522f3a7e6218a3d
SrivenkatanathanDevOps <srivenkat2206@gmail.com>
2026-03-07 16:46:24 +0530

    commiting the 2nd phase of commands

--- frontend.sh
+++ frontend.sh
@@ -10,3 +10,15 @@ dnf install -y nodejs
 
 node --version
 npm --version
+
+curl -L -o /tmp/frontend.tar.gz https://raw.githubusercontent.com/raghudevopsb88/wealth-project/main/artifacts/frontend.tar.gz
+mkdir -p /tmp/frontend
+cd /tmp/frontend
+tar xzf /tmp/frontend.tar.gz
+
+cd /tmp/frontend
+npm ci
+npm run build
+
+rm -rf /usr/share/nginx/html/*
+cp -r /tmp/frontend/dist/* /usr/share/nginx/html/
