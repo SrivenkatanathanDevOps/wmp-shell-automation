daaece31458a8ffe1e704381f0e726a986ae6f71
SrivenkatanathanDevOps <srivenkat2206@gmail.com>
2026-03-08 09:24:05 +0530

    moving the auth-service.service file after curl

--- auth.sh
+++ auth.sh
@@ -6,11 +6,12 @@ useradd -r -s /bin/false appuser
 
 mkdir -p /app
 
-cp auth-service.service /etc/systemd/system/auth-service.service
 
 curl -L -o /tmp/auth-service.tar.gz https://raw.githubusercontent.com/raghudevopsb88/wealth-project/main/artifacts/auth-service.tar.gz
 cd /app
 tar xzf /tmp/auth-service.tar.gz
+
+cp auth-service.service /etc/systemd/system/auth-service.service
 
 cd /app
 CGO_ENABLED=0 go build -o auth-service ./cmd/server
